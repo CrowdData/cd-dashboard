444ef9fcf34fb0c81f7995155df660beae532000
sankrantijoshi <sankrantijoshi@gmail.com>
2015-04-11 03:48:00 +0530

    Update demand-view.php

--- demand-view.php
+++ demand-view.php
@@ -8,14 +8,14 @@
 	$analytics=new Template("templates/template-analytics.tpl");
 
     
-   $layout->set("pageTitle", "Demand View");
+   $layout->set("pageTitle", "TumTum Demand");
    $layout->set("cssfilename","dashboard-demand");
    $layout->set("jsfilename","demand");
    $layout->set("menu", $menu->output());
     $layout->set("header","TumTum Demand");
 
     $layout->set("analytics",$analytics->output());
-    $layout->set("tableMessage","This table displays latest demand on the bus stops.");
+    $layout->set("tableMessage","The table below shows the demand for TumTums, as reported by other users.  Think this is missing some data? Please <a href=demand-report.php> let us know about it</a>.");
 
     $layout->set("buttonredirect", "demand-report.php");
 	$layout->set("button","Report currect bus stop demand");
