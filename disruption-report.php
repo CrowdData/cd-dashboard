3ca88ea1a3e7c9f73dd88b324ea2284061b26bdf
sankrantijoshi <sankrantijoshi@gmail.com>
2015-04-11 03:48:55 +0530

    Update disruption-report.php

--- disruption-report.php
+++ disruption-report.php
@@ -6,11 +6,11 @@
 	$layout = new Template("templates/report.tpl");
     $analytics=new Template("templates/template-analytics.tpl");
 
-    $layout->set("datasetID","incidentv2");
+    $layout->set("datasetID","incidents");
     $layout->set("templateID","http://crowddata.abdn.ac.uk/def/incidents/Report");
     $layout->set("headerContent","Provide details of an incident.");
     $layout->set("pageTitle", "Incident report");
-	$layout->set("templateMessage","Please use the form below to provide details of an incident (unexpected event).");
+	$layout->set("templateMessage","Please use this form to let other users know about an incident on campus, such as road closure, internet outage, or panther sighting that they should be aware of.");
 	$layout->set("cssfilename", "dashboard-disruption");
     $layout->set("button","Report incident");
 	
